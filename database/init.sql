f28449d293a6b1ae9122faefcbc88c057d9fc7e5
Pedro Nascimento <pedrovitor.marques@gmail.com>
2023-07-18 14:55:36 -0300

    Adicionando Configuração de Banco PostgreSQL

---
+++ database/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE registro_brt (
+    id                      SERIAL PRIMARY KEY,
+    codigo                  VARCHAR(50) NOT NULL,
+    placa                   VARCHAR(7),
+    linha                   VARCHAR(10),
+    latitude                DECIMAL NOT NULL,
+    longitude               DECIMAL NOT NULL,
+    datahora                TIMESTAMP,
+    velocidade              DECIMAL,
+    id_migracao_trajeto     INTEGER,
+    sentido                 VARCHAR(10),
+    trajeto                 VARCHAR(100),
+    hodometro               DECIMAL,
+    direcao                 VARCHAR(10)
+);
